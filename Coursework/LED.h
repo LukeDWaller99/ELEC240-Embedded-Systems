b1bcce6c560389e70ac0b11536f890f112ac9ca3
Luke Waller <ldwaller@SOCNET.SOC.PLYMOUTH.AC.UK>
2019-11-16 12:56:20 +0000

    Initilise all of the onboard LEDs, Timers 2, 3, and 4, and the IRQ Handlers

--- Coursework/LED.h
+++ Coursework/LED.h
@@ -1,8 +1,5 @@
 #include <stm32f4xx.h>
 
-void init_green_LED		(void);
-void init_red_LED			(void);
-void init_blue_LED		(void);
 void init_all_LED		(void);
 void toggle_green_LED (void);
 void toggle_red_LED 	(void);
